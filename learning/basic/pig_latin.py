9ec4546be33d0462b7d72a5c8066eccf5d343e0c
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-08-26 13:43:22 -0600

    Add a pig latin parser

---
+++ learning/basic/pig_latin.py
@@ -0,0 +1,19 @@
+def pig_latin(sentence):
+    words = sentence.split(" ")
+    pig_latin_sentence = ""
+    for word in words:
+        pig_latin_sentence += pig_latin_word(word)+" "
+    return (pig_latin_sentence.strip())
+
+def pig_latin_word(word):
+    if not isinstance(word, str):
+        return "Word must be a string"
+    vowels = ['a','e','i','o','u']
+    if word[0] in vowels:
+        return f"{word}-way"
+    
+    word_list = list(word);
+    first_letter = word_list.pop(0)
+    return "".join(word_list) + f"-{first_letter}ay"
+
+print(pig_latin("Well it looks like that works"))
