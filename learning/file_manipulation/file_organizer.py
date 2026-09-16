3b263bc60b167a86f4491a549533e8687612dde9
Josh Sanford <13478602+lyzboy@users.noreply.github.com>
2026-09-16 13:39:44 -0600

    Add file organizing script
    Currently learning/testing os and pathlib functions.

---
+++ learning/file_manipulation/file_organizer.py
@@ -0,0 +1,14 @@
+import os
+import pathlib
+
+def list_files(path: str)->list | str:
+    try:
+        parsed_path = pathlib.PurePath(path)
+        local_path = pathlib.Path(parsed_path)
+        files = os.listdir(local_path);
+        return files
+    except FileNotFoundError:
+        return f"Files not found or bad path"
+
+path = input("What is the folder to organize?\n")
+print(list_files(path))
